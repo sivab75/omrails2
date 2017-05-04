6074b795042913f74601e55cc8b09511bd58e9b8
sivabalan <sivabalan_msc07@yahoo.com>
2017-05-04 20:41:42 +0800

    creation of webpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+root "pages#home"
+
+get 'about' => 'pages#about' 
+get 'contactus' => 'pages#contactus' 
+get 'myteam' => 'pages#myteam' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
