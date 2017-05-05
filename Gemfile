552e47d97c0081e0a659ed4e82858d04542c5aec
sivabalan <sivabalan_msc07@yahoo.com>
2017-05-05 21:54:18 +0800

    devise user

--- Gemfile
+++ Gemfile
@@ -29,7 +29,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+#aunthentication setting
+gem 'devise', git: 'https://github.com/gogovan/devise.git' , branch: 'rails-5.1'
+gem 'erubis'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  #get 'pages/home'
+devise_for :users
+as :user do
+	get 'signin' => 'devise/sessions#new'
+	delete 'signout' => 'devise/sessions#destroy'
+	get 'signup' => 'devise/registrations#new'
+end	 
 root "pages#home"
 
 get 'about' => 'pages#about' 
 get 'contactus' => 'pages#contactus' 
 get 'myteam' => 'pages#myteam' 
+get 'courses' => 'pages#courses' 
+get 'learnmore' => 'pages#learnmore' 
+get 'membership' => 'pages#membership' 
+get 'seminars' => 'pages#seminars' 
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
